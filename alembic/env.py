02d19edb0f7204e6da34019732bea5ca460e2de7
olegmenkov <olegmenkov.rk@gmail.com>
2024-12-02 22:11:02 +0300

    for initial setup

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,13 @@
+import os
+import sys
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from app.db_setup import Base
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # this is the Alembic Config object, which provides access to the values within the .ini file in use.
 config = context.config
