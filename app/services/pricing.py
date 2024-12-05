6135af7b7a21ee38cb93d84f08640b8696fc5236
Boyarskiy Andrew <aboyarskiy@team.amocrm.com>
2024-12-05 19:38:31 +0300

    fix

--- app/services/pricing.py
+++ app/services/pricing.py
@@ -1,8 +1,11 @@
+import os
+
+
 def calculate_price(area, rooms, floor, total_floors, district, underground) -> float:
-    scaler_path='/datascaler.pkl'
-    model_path='/model.pkl'
-    district_clusters_path='/district_clusters.pkl'
-    underground_clusters_path='/underground_clusters.pkl'
+    scaler_path = 'app/services/data/scaler.pkl'
+    model_path = 'app/services/data/model.pkl'
+    district_clusters_path = 'app/services/data/district_clusters.pkl'
+    underground_clusters_path = 'app/services/data/underground_clusters.pkl'
     """
     Функция для предсказания стоимости квартиры.
 
